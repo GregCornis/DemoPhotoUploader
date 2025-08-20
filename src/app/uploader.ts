95c1a1c921456a1e6b44adc4cc0d5e7132085bb9
Gregoire <gregoire.chalony@sites.fr>
2025-08-20 10:39:42 +0200

    Upload files directly from browser
    Using presigned_urls

--- src/app/uploader.ts
+++ src/app/uploader.ts
@@ -1,5 +1,6 @@
-//import { S3Client, ListBucketsCommand, ListObjectsV2Command } from "@aws-sdk/client-s3";
+import { GetObjectAttributesCommand, ListObjectsV2Command, PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { openDB, getFromStore, Credentials, UploadData, sleep } from "./utils";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 
 let _credentials: Credentials | null = null
@@ -26,6 +27,7 @@ onmessage = async (e: MessageEvent<UploadData>) => {
     console.log("Message received from main script", e.data)
 
     const credentials = await getCredentials()
+    console.log("Credentials:", credentials);
 
     await uploadAll(e.data.files, credentials, e.data.name)
 };
@@ -38,9 +40,17 @@ async function uploadAll(files: File[], credentials: Credentials, prefix: string
         return
     }
 
+    const client = new S3Client({
+        region: "us-east-1",
+        credentials: {
+            accessKeyId: credentials.accessKeyId,
+            secretAccessKey: credentials.secretAccessKey
+        }
+    })
+
     let uploaded = 0
     files.forEach(async (f) => {
-        await upload(f, credentials, prefix)
+        await upload(f, prefix, client)
         // await sleep(10000);
         console.log("Uploaded", f);
         uploaded += 1
@@ -48,16 +58,24 @@ async function uploadAll(files: File[], credentials: Credentials, prefix: string
     })
 }
 
-async function upload(file: File, credentials: Credentials, prefix: string) {
-    const reader = new FileReader()
-    const url = new URL("/upload", location.origin)
-    url.searchParams.append("path", `${prefix}/${file.webkitRelativePath}`)
-    url.searchParams.append("accessKeyId", credentials.accessKeyId)
-    url.searchParams.append("secretAccessKey", credentials.secretAccessKey)
+
+async function upload(file: File, prefix: string, client: S3Client) {
+    console.log("Uploading", file.name);
+    const path = encodeURI(`${prefix}/${file.webkitRelativePath}`);
+
+    const command = new PutObjectCommand({
+        Bucket: "cornis-drone-photos",
+        Key: `test/${path}`
+    })
+
+    const url = await getSignedUrl(client, command, { expiresIn: 3600 });
+    console.log("Presigned URL:", url)
 
     const resp = await fetch(url, {
-        method: "POST",
+        method: "PUT",
+        headers: { "Content-Type": file.type },
         body: file
     })
-    console.log("Resp", resp);
+    console.log("Created file", resp)
 }
+
